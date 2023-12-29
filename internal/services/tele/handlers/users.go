108e7a70ba7ed677465b117b23c8363e2ceed6ee
thuanle <thuanle@hcmut.edu.vn>
2023-12-29 10:31:19 +0700

    add debug

--- internal/services/tele/handlers/users.go
+++ internal/services/tele/handlers/users.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/rs/zerolog/log"
 	"gopkg.in/telebot.v3"
 	"strings"
 	"thuanle/cse-mark/internal/data"
@@ -51,6 +52,10 @@ func GetMyProfile(c telebot.Context) error {
 func GetStudentProfile(c telebot.Context) error {
 	chatId := c.Chat().ID
 
+	log.Info().
+		Int64("chatId", chatId).
+		Msg("Get student profile")
+
 	marks, err := data.FetchMarksByChatId(chatId)
 	if err != nil {
 		return err
@@ -62,6 +67,11 @@ func GetStudentProfile(c telebot.Context) error {
 
 func GetTeacherProfile(c telebot.Context) error {
 	chatUsername := c.Chat().Username
+
+	log.Info().
+		Str("chatUsername", chatUsername).
+		Msg("Get teacher profile")
+
 	courses, err := data.FetchTeacherCourses(chatUsername)
 	if err != nil {
 		return err
